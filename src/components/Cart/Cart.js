e4a914413ca92ed66f9e48c67ccda36d6299b8db
Ashkan Rezaei <33765442+ashkan-rezaei-tsh@users.noreply.github.com>
2022-02-02 16:08:57 +0330

    added removeItem logic

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,11 +1,36 @@
+import { useContext } from "react";
+
 import Modal from "../UI/Modal";
+import CartItem from "./CartItem";
 import classes from "./Cart.module.css";
 
+import CartContext from "../../store/cart-context";
+
 const Cart = (props) => {
+    const cartCtx = useContext(CartContext);
+
+    const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+    const hasItem = cartCtx.items.length > 0;
+
+    const addItemHandler = (item) => {
+        cartCtx.addItem({ ...item, amount: 1 });
+    };
+
+    const removeItemHandler = (id) => {
+        cartCtx.removeItem(id);
+    };
+
     const cartItems = (
         <ul className={classes["cart-items"]}>
-            {[{ id: "c1", name: "sushi", amount: 12.99, count: 2 }].map((item) => (
-                <li key={item.id}>{item.name}</li>
+            {cartCtx.items.map((item) => (
+                <CartItem
+                    key={item.id}
+                    name={item.name}
+                    price={item.price}
+                    amount={item.amount}
+                    onAdd={addItemHandler.bind(null, item)}
+                    onRemove={removeItemHandler.bind(null, item.id)}
+                />
             ))}
         </ul>
     );
@@ -15,13 +40,13 @@ const Cart = (props) => {
             {cartItems}
             <div className={classes.total}>
                 <span>Total Amount</span>
-                <span>35.62</span>
+                <span>{totalAmount}</span>
             </div>
             <div className={classes.actions}>
                 <button className={classes["button--alt"]} onClick={props.onClose}>
                     Close
                 </button>
-                <button className={classes.button}>Order</button>
+                {hasItem && <button className={classes.button}>Order</button>}
             </div>
         </Modal>
     );
--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -9,16 +9,46 @@ const defaultCartState = {
 
 const cartReducer = (state, action) => {
     if (action.type === "ADD_ITEM") {
-        const updatedItems = state.items.concat(action.item);
         const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+
+        const existingCartItemIndex = state.items.findIndex((item) => item.id === action.item.id);
+        const existingCartItem = state.items[existingCartItemIndex];
+
+        let updatedItems;
+
+        if (existingCartItem) {
+            const updatedItem = { ...existingCartItem, amount: existingCartItem.amount + action.item.amount };
+            updatedItems = [...state.items];
+            updatedItems[existingCartItemIndex] = updatedItem;
+        } else {
+            updatedItems = state.items.concat(action.item);
+        }
+
         return {
             items: updatedItems,
             totalAmount: updatedTotalAmount,
         };
     } else if (action.type === "REMOVE_ITEM") {
-    } else {
-        return defaultCartState;
+        const existingCartItemIndex = state.items.findIndex((item) => item.id === action.id);
+        const existingCartItem = state.items[existingCartItemIndex];
+
+        const updatedTotalAmount = state.totalAmount - existingCartItem.price;
+
+        let updatedItems;
+
+        if (existingCartItem.amount === 1) {
+            updatedItems = state.items.filter((item) => item.id !== action.id);
+        } else {
+            updatedItems = [...state.items];
+            updatedItems[existingCartItemIndex] = { ...existingCartItem, amount: existingCartItem.amount - 1 };
+        }
+
+        return {
+            items: updatedItems,
+            totalAmount: updatedTotalAmount,
+        };
     }
+    return defaultCartState;
 };
 
 const CartProvider = (props) => {
